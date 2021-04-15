c2167a8456f3b5c3277896ed9646a975d1fdd892
Yiya <houhoucoop@gmail.com>
2021-04-15 23:55:14 +0800

    Updates

---
+++ precache-manifest.d893ee433e039c9361732d61977c0943.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af35f6624bc5f7bcb442ab38c033bb7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ab0eb9d73a9126b164d",
+    "url": "./static/css/main.e8354f9a.chunk.css"
+  },
+  {
+    "revision": "ea336d778d7a97972274",
+    "url": "./static/js/2.2ca300e4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.2ca300e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab0eb9d73a9126b164d",
+    "url": "./static/js/main.d1644277.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.d1644277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ebb8d3f2c0e0888213a",
+    "url": "./static/js/runtime-main.1d4bf3f6.js"
+  }
+]);
